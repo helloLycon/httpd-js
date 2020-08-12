3a1687682049bff11228f0a2a7b3b7c98bcafaf2
lycon <lycon123@foxmail.com>
2020-08-12 13:49:12 +0800

    https server

--- httpd.js
+++ httpd.js
@@ -1,20 +1,38 @@
 #!/usr/bin/node
 
-var http = require('http');
-http.createServer(function (req, res) {
+const httpsPort = 4430;
+const httpPort = 8080;
+
+const http = require('http');
+const https = require('https');
+const fs = require('fs');
+
+const options = {
+  key: fs.readFileSync('./key.pem'),
+  cert: fs.readFileSync('./cert.pem')
+};
+
+
+function helloWorld(req, res) {
+  res.writeHead(200);
+  res.end("hello world\n");
+}
+
+function acceptJson(req, resp) {
   var jsonData = "";
   req.on('data', function (chunk) {
     jsonData += chunk;
   });
   req.on('end', function () {
     var reqObj = JSON.parse(jsonData);
-    /*var resObj = {
-      message: "Hello " + reqObj.name,
-      question: "Are you a good " + reqObj.occupation + "?"
-    };
-    res.writeHead(200);
-    res.end(JSON.stringify(resObj));*/
     console.log(reqObj);
+    resp.writeHead(200, { 'Content-Type': 'application/json' });
+    resp.end('{"errno": 0,"cost": "20.00"}');
   });
-}).listen(6969);
+}
 
+https.createServer(options, helloWorld).listen(httpsPort);
+console.log('httpd listen on ' + httpsPort + '.');
+
+//http.createServer(helloWorld).listen(httpPort);
+//console.log('httpd listen on ' + httpPort + '.');
